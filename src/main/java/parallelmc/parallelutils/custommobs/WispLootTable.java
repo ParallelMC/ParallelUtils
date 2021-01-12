ac76ae7853ac919df7ecf487cc85cf122492a755
Evelyn Dube <ecdgirl15@gmail.com>
2021-01-12 18:24:42 -0500

    Added custom loot table for Wisp

--- src/main/java/parallelmc/parallelutils/custommobs/WispLootTable.java
+++ src/main/java/parallelmc/parallelutils/custommobs/WispLootTable.java
@@ -1,8 +1,12 @@
 package parallelmc.parallelutils.custommobs;
 
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.loot.LootContext;
 import org.bukkit.loot.LootTable;
 import org.bukkit.plugin.Plugin;
@@ -22,18 +26,34 @@ public class WispLootTable implements LootTable {
     @NotNull
     @Override
     public Collection<ItemStack> populateLoot(@NotNull Random random, @NotNull LootContext lootContext) {
-        //TODO: ask DB how soul shards work so I can make an ItemStack of them
-        return new ArrayList<>();
+        ArrayList<ItemStack> loot = new ArrayList<>();
+        loot.add(createShard());
+        return loot;
     }
 
     @Override
     public void fillInventory(@NotNull Inventory inventory, @NotNull Random random, @NotNull LootContext lootContext) {
+        inventory.addItem(createShard());
+    }
 
+    private ItemStack createShard(){
+        ItemStack shard = new ItemStack(Material.PRISMARINE_SHARD, 1);
+        try {
+            ItemMeta shardMeta = shard.getItemMeta();
+            shardMeta.setDisplayName("&fSoul Shard");
+            shardMeta.setCustomModelData(1000001);
+            shardMeta.addEnchant(Enchantment.DURABILITY, 1, true);
+            shardMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        }
+        catch(NullPointerException e){
+            e.printStackTrace();
+        }
+        return shard;
     }
 
     @NotNull
     @Override
     public NamespacedKey getKey() {
-        return new NamespacedKey(plugin, "");
+        return new NamespacedKey(plugin, "soul-shard-item");
     }
 }
