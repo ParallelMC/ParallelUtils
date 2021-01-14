add2121245e5b6ccda8277d2947f38e1246e6bc7
jakebacker <jbacker42@gmail.com>
2021-01-14 15:18:20 -0500

    Add commands/permissions structure

--- src/main/java/parallelmc/parallelutils/Parallelutils.java
+++ src/main/java/parallelmc/parallelutils/Parallelutils.java
@@ -1,8 +1,21 @@
 package parallelmc.parallelutils;
 
+import net.minecraft.server.v1_16_R1.EntityPlayer;
+import net.minecraft.server.v1_16_R1.EntityZombie;
+import net.minecraft.server.v1_16_R1.PathfinderGoalMeleeAttack;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Zombie;
+import org.bukkit.loot.LootTable;
 import org.bukkit.plugin.java.JavaPlugin;
 import parallelmc.parallelutils.commands.Commands;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 public final class Parallelutils extends JavaPlugin {
 
@@ -21,11 +34,14 @@ public final class Parallelutils extends JavaPlugin {
 		// More startup logic here
 
 
+
 		// Setup commands
 		Commands commands = new Commands(this);
 
 		getCommand("parallelutils").setExecutor(commands);
+		getCommand("parallelutils").setTabCompleter(commands);
 		getCommand("pu").setExecutor(commands);
+		getCommand("pu").setTabCompleter(commands);
 	}
 
 	@Override
