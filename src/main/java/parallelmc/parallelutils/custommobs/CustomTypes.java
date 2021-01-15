0dd1ae3d3a38123c087195c540f705ecce1fb1bb
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2021-01-14 21:47:06 -0500

    Make the CustomTypes stuff work Welcome to Reflection and Unsafe hell. I hate Java

--- src/main/java/parallelmc/parallelutils/custommobs/CustomTypes.java
+++ src/main/java/parallelmc/parallelutils/custommobs/CustomTypes.java
@@ -1,10 +1,12 @@
 package parallelmc.parallelutils.custommobs;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import sun.misc.Unsafe;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
+import java.lang.reflect.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,10 +17,41 @@ public class CustomTypes {
 
     }
 
-    public void addEntityType(String name, Class<? extends Entity> entity, short id) throws Exception{
-        Constructor<EntityType> constructor = EntityType.class.getDeclaredConstructor(String.class, Class.class, int.class);
+    public void addEntityType(String name, Class<? extends Entity> entity, short id) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+        Constructor<?> constructor = Unsafe.class.getDeclaredConstructors()[0];
         constructor.setAccessible(true);
-        EntityType type = constructor.newInstance(name, entity, id);
+
+        Unsafe unsafe = (Unsafe) constructor.newInstance();
+        EntityType type = (EntityType) unsafe.allocateInstance(EntityType.class);
+
+        Field ordinalField = Enum.class.getDeclaredField("ordinal");
+        makeAccessible(ordinalField);
+        ordinalField.setInt(type, 99);
+
+        Field nameField = Enum.class.getDeclaredField("name");
+        makeAccessible(nameField);
+        nameField.set(type, name);
+
+        Field entityClassField = EntityType.class.getDeclaredField("clazz");
+        makeAccessible(entityClassField);
+        entityClassField.set(type, entity);
+
+        Field entityIdField = EntityType.class.getDeclaredField("typeId");
+        makeAccessible(entityIdField);
+        entityIdField.set(type, id);
+
+        Field indepField = EntityType.class.getDeclaredField("independent");
+        makeAccessible(indepField);
+        indepField.set(type, true);
+
+        Field livingField = EntityType.class.getDeclaredField("living");
+        makeAccessible(livingField);
+        livingField.set(type, entity != null && LivingEntity.class.isAssignableFrom(entity));
+
+        Field keyField = EntityType.class.getDeclaredField("key");
+        makeAccessible(keyField);
+        keyField.set(type, name == null ? null : NamespacedKey.minecraft(name));
+
 
         Field field = EntityType.class.getDeclaredField("NAME_MAP");
         field.setAccessible(true);
@@ -35,5 +68,12 @@ public class CustomTypes {
 
     public EntityType getType(String name){
         return types.get(name);
+    }
+
+    private static void makeAccessible(Field field) throws NoSuchFieldException, IllegalAccessException {
+        field.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~ Modifier.FINAL);
     }
 }
