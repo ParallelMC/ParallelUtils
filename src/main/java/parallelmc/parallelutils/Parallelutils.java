da21e992337221e7d466397e7e0b6de06498b213
jakebacker <jbacker42@gmail.com>
2021-01-14 21:47:06 -0500

    Make the CustomTypes stuff work Welcome to Reflection and Unsafe hell. I hate Java

--- src/main/java/parallelmc/parallelutils/Parallelutils.java
+++ src/main/java/parallelmc/parallelutils/Parallelutils.java
@@ -3,14 +3,19 @@ package parallelmc.parallelutils;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import parallelmc.parallelutils.commands.Commands;
+import parallelmc.parallelutils.custommobs.CustomTypes;
+import parallelmc.parallelutils.custommobs.EntityWisp;
 
 public final class Parallelutils extends JavaPlugin {
 
 	String baseDataFolder = this.getDataFolder().getAbsolutePath();
 	FileConfiguration config = this.getConfig();
 
+	static CustomTypes mobTypes;
+
 	@Override
 	public void onEnable() {
+		mobTypes = new CustomTypes();
 		// Plugin startup logic
 		this.saveDefaultConfig();
 
@@ -19,7 +24,14 @@ public final class Parallelutils extends JavaPlugin {
 		saveConfig();
 
 		// More startup logic here
+		short id = 1234;
+		try {
+			mobTypes.addEntityType("wisp", EntityWisp.class, id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
+		System.out.println(mobTypes.getType("wisp"));
 
 		// Setup commands
 		Commands commands = new Commands(this);
