046a43566e941cf883d7311f24e762a58f905d4b
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2021-01-21 14:56:19 -0500

    It works!

--- src/main/java/parallelmc/parallelutils/custommobs/NMSWisp.java
+++ src/main/java/parallelmc/parallelutils/custommobs/NMSWisp.java
@@ -11,29 +11,12 @@ import java.util.Set;
 public class NMSWisp extends EntityZombie {
     public NMSWisp(EntityTypes<? extends EntityZombie> entitytypes, World world) {
         super(entitytypes, world);
-        goalSetting();
+        initPathfinder();
     }
 
     public NMSWisp(World world) {
         super(world);
-        goalSetting();
-    }
-
-    private void goalSetting(){
-        //clearing Zombie goals
-        Map goalC = (Map)getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
-        goalC.clear();
-        Set goalD = (Set) getPrivateField("d", PathfinderGoalSelector.class, goalSelector);
-        goalD.clear();
-        Map targetC = (Map)getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
-        targetC.clear();
-        Set targetD = (Set)getPrivateField("d", PathfinderGoalSelector.class, targetSelector);
-        targetD.clear();
-
-        this.goalSelector.a(0, new PathfinderGoalMeleeAttack(this,1.0, false));
-        this.goalSelector.a(1, new PathfinderGoalRandomStroll(this, 1.0));
-
-        this.targetSelector.a(0, new PathfinderGoalHurtByTarget(this));
+        initPathfinder();
     }
 
     public static Object getPrivateField(String fieldName, Class clazz, Object object)
@@ -52,5 +35,40 @@ public class NMSWisp extends EntityZombie {
             e.printStackTrace();
         }
         return o;
+    }
+
+    @Override
+    public void initPathfinder() {
+        //clearing Zombie goals
+        Map goalC = (Map)getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
+        goalC.clear();
+        Set goalD = (Set) getPrivateField("d", PathfinderGoalSelector.class, goalSelector);
+        goalD.clear();
+        Map targetC = (Map)getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
+        targetC.clear();
+        Set targetD = (Set)getPrivateField("d", PathfinderGoalSelector.class, targetSelector);
+        targetD.clear();
+
+        this.goalSelector.a(0, new PathfinderGoalMeleeAttack(this,1.0, false));
+        this.goalSelector.a(1, new PathfinderGoalRandomStroll(this, 1.0));
+
+        this.targetSelector.a(0, new PathfinderGoalHurtByTarget(this));
+    }
+
+    public static void initPathfinder(EntityZombie zombie) {
+        //clearing Zombie goals
+        Map goalC = (Map)getPrivateField("c", PathfinderGoalSelector.class, zombie.goalSelector);
+        goalC.clear();
+        Set goalD = (Set) getPrivateField("d", PathfinderGoalSelector.class, zombie.goalSelector);
+        goalD.clear();
+        Map targetC = (Map)getPrivateField("c", PathfinderGoalSelector.class, zombie.targetSelector);
+        targetC.clear();
+        Set targetD = (Set)getPrivateField("d", PathfinderGoalSelector.class, zombie.targetSelector);
+        targetD.clear();
+
+        zombie.goalSelector.a(0, new PathfinderGoalMeleeAttack(zombie,1.0, false));
+        zombie.goalSelector.a(1, new PathfinderGoalRandomStroll(zombie, 1.0));
+
+        zombie.targetSelector.a(0, new PathfinderGoalHurtByTarget(zombie));
     }
 }
