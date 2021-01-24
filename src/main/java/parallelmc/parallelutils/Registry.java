c79e4284eeea51f6db7ae385ebaab426e3924e71
dubiousace <dubiousace.parallel@gmail.com>
2021-01-24 01:52:10 -0500

    Add particles for Wisp???

--- src/main/java/parallelmc/parallelutils/Registry.java
+++ src/main/java/parallelmc/parallelutils/Registry.java
@@ -4,6 +4,7 @@ import net.minecraft.server.v1_16_R3.Entity;
 import net.minecraft.server.v1_16_R3.EntityInsentient;
 import org.bukkit.Bukkit;
 import parallelmc.parallelutils.custommobs.EntityPair;
+import parallelmc.parallelutils.custommobs.ParticleData;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -13,10 +14,19 @@ public class Registry {
 
 	private static HashMap<String, EntityPair> entities = new HashMap<>();
 
+	private static HashMap<String, ParticleData> entityParticles = new HashMap<>();
+
 	public static void registerEntity(String uuid, String type, Entity entity) {
 		Bukkit.getLogger().log(Level.INFO, "[ParallelUtils] Registering entity " + uuid);
 		entities.put(uuid, new EntityPair(type, entity));
 	}
+
+	public static void registerParticles(String type,ParticleData data) {
+		Bukkit.getLogger().log(Level.INFO, "[ParallelUtils] Registering particles for " + type);
+		entityParticles.put(type, data);
+	}
+
+	public static ParticleData getParticleData(String type){ return entityParticles.get(type);}
 
 	public static EntityPair getEntity(String uuid) {
 		return entities.get(uuid);
@@ -35,7 +45,7 @@ public class Registry {
 		return entities.remove(uuid);
 	}
 
-	public static boolean contains(String uuid) {
+	public static boolean containsEntity(String uuid) {
 		Bukkit.getLogger().log(Level.ALL, "[ParallelUtils] Does contain " + uuid);
 		return entities.containsKey(uuid);
 	}
