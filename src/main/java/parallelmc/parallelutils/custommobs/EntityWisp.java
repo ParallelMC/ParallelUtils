8aab82b2ed214f0893e93c177da0d9cf698baf26
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2021-01-26 22:00:39 -0500

    Move event listeners

--- src/main/java/parallelmc/parallelutils/custommobs/EntityWisp.java
+++ src/main/java/parallelmc/parallelutils/custommobs/EntityWisp.java
@@ -1,61 +1,115 @@
 package parallelmc.parallelutils.custommobs;
 
+
+import net.minecraft.server.v1_16_R3.*;
+import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_16_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftEntity;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftZombie;
-import org.jetbrains.annotations.NotNull;
-import org.bukkit.entity.EntityType;
-import org.bukkit.plugin.Plugin;
-import parallelmc.parallelutils.Parallelutils;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+import parallelmc.parallelutils.Registry;
 
-import static org.bukkit.attribute.Attribute.*;
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.Set;
 
-public class EntityWisp extends CraftZombie{
-    Plugin plugin;
-
-    public EntityWisp(CraftServer server, NMSWisp entity, Plugin plugin) {
-        super(server, entity);
-        this.plugin = plugin;
-        setupNBT(plugin, this);
+public class EntityWisp extends EntityZombie {
+    public EntityWisp(EntityTypes<? extends EntityZombie> entitytypes, World world) {
+        super(entitytypes, world);
+        initPathfinder();
     }
 
-    @NotNull
+    public EntityWisp(World world) {
+        super(world);
+        initPathfinder();
+    }
+
+    public static Object getPrivateField(String fieldName, Class clazz, Object object)
+    {
+        Field field;
+        Object o = null;
+
+        try {
+            field = clazz.getDeclaredField(fieldName);
+
+            field.setAccessible(true);
+
+            o = field.get(object);
+        }
+        catch(NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return o;
+    }
+
     @Override
-    public EntityType getType(){
-        return Parallelutils.mobTypes.getType("wisp");
+    public void initPathfinder() {
+        //clearing Zombie goals
+        Map goalC = (Map)getPrivateField("c", PathfinderGoalSelector.class, goalSelector);
+        goalC.clear();
+        Set goalD = (Set) getPrivateField("d", PathfinderGoalSelector.class, goalSelector);
+        goalD.clear();
+        Map targetC = (Map)getPrivateField("c", PathfinderGoalSelector.class, targetSelector);
+        targetC.clear();
+        Set targetD = (Set)getPrivateField("d", PathfinderGoalSelector.class, targetSelector);
+        targetD.clear();
+
+        this.goalSelector.a(0, new PathfinderGoalMeleeAttack(this,1.0, false));
+        this.goalSelector.a(1, new PathfinderGoalRandomStroll(this, 1.0));
+
+        this.targetSelector.a(0, new PathfinderGoalHurtByTarget(this));
     }
 
-    public static void setupNBT(Plugin plugin, CraftZombie entity){
-        //Attributes
-        //Health = 30
-        entity.getAttribute(GENERIC_MAX_HEALTH).setBaseValue(30.0);
-        entity.setHealth(entity.getAttribute(GENERIC_MAX_HEALTH).getBaseValue());
+    @Override
+    protected boolean T_() {
+        return false;
+    }
 
-        //Damage = 8
-        entity.getAttribute(GENERIC_ATTACK_DAMAGE).setBaseValue(8.0);
+    public static void initPathfinder(EntityZombie zombie) {
+        //clearing Zombie goals
+        Map goalC = (Map)getPrivateField("c", PathfinderGoalSelector.class, zombie.goalSelector);
+        goalC.clear();
+        Set goalD = (Set) getPrivateField("d", PathfinderGoalSelector.class, zombie.goalSelector);
+        goalD.clear();
+        Map targetC = (Map)getPrivateField("c", PathfinderGoalSelector.class, zombie.targetSelector);
+        targetC.clear();
+        Set targetD = (Set)getPrivateField("d", PathfinderGoalSelector.class, zombie.targetSelector);
+        targetD.clear();
 
-        //Follow range = 16
-        entity.getAttribute(GENERIC_FOLLOW_RANGE).setBaseValue(16.0);
+        zombie.goalSelector.a(0, new PathfinderGoalMeleeAttack(zombie,1.0, false));
+        zombie.goalSelector.a(1, new PathfinderGoalRandomStroll(zombie, 1.0));
 
-        //Movement speed = 0.25
-        entity.getAttribute(GENERIC_MOVEMENT_SPEED).setBaseValue(0.25);
+        zombie.targetSelector.a(0, new PathfinderGoalHurtByTarget(zombie));
+    }
 
-        //No zombie reinforcements
-        entity.getAttribute(ZOMBIE_SPAWN_REINFORCEMENTS).setBaseValue(0);
+    public static EntityWisp spawn(JavaPlugin plugin, CraftServer server, CraftWorld world, Location l) {
+        EntityWisp wisp = new EntityWisp(world.getHandle());
+        CraftZombie zombie = (CraftZombie) CraftEntity.getEntity(server, wisp);
 
-        //Other stuff
+        setup(plugin, zombie);
+        wisp.setPosition(l.getX(), l.getY(), l.getZ());
+        world.getHandle().addEntity(wisp, CreatureSpawnEvent.SpawnReason.CUSTOM);
 
-        //invis
-        entity.setInvisible(true);
+        Registry.registerEntity(zombie.getUniqueId().toString(), "wisp", wisp);
 
-        //can't pickup items
-        entity.setCanPickupItems(false);
+        return wisp;
+    }
 
-        //no random equipment
-        entity.getEquipment().clear();
+    public static EntityZombie setup(JavaPlugin plugin, CraftZombie mob) {
+        CraftWisp.setupNBT(plugin, mob);
 
-        entity.setShouldBurnInDay(false);
+        EntityZombie wisp = mob.getHandle();
 
-        //silent
-        entity.setSilent(true);
+        EntityWisp.initPathfinder(wisp);
+
+        if(!Registry.particleTaskRunning){
+            BukkitTask task = new ParticleTask(plugin).runTaskTimer(plugin, 0, 10);
+            Registry.particleTaskRunning = true;
+        }
+
+        return wisp;
     }
 }
---
+++ src/main/java/parallelmc/parallelutils/custommobs/events/CustomMobsGeneralEntityListener.java
@@ -0,0 +1,23 @@
+package parallelmc.parallelutils.custommobs.events;
+
+import com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import parallelmc.parallelutils.Registry;
+
+import java.util.logging.Level;
+
+/** For non-specific entity listeners **/
+public class CustomMobsGeneralEntityListener implements Listener {
+
+	@EventHandler
+	public void onEntityDespawn(final EntityRemoveFromWorldEvent event) {
+		CraftEntity entity = (CraftEntity)event.getEntity();
+		if (Registry.containsEntity(entity.getUniqueId().toString())) {
+			Bukkit.getLogger().log(Level.ALL, "[ParallelUtils] Removing entity " + entity.getUniqueId().toString() + " from world");
+			Registry.removeEntity(entity.getUniqueId().toString());
+		}
+	}
+}
