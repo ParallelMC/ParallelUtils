2ef56bbf25be001c32b8410c869f9589210fb4da
dubiousace <dubiousace.parallel@gmail.com>
2021-02-03 20:56:31 -0500

    Add leash infrastructure to SpawnerRegistry

--- src/main/java/parallelmc/parallelutils/custommobs/registry/SpawnerRegistry.java
+++ src/main/java/parallelmc/parallelutils/custommobs/registry/SpawnerRegistry.java
@@ -4,6 +4,7 @@ import org.bukkit.Location;
 import parallelmc.parallelutils.Parallelutils;
 import parallelmc.parallelutils.custommobs.spawners.SpawnerOptions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Level;
 
@@ -12,6 +13,13 @@ public class SpawnerRegistry {
 	private HashMap<String, SpawnerOptions> spawners;
 
 	private HashMap<Location, Integer> mobCounts;
+
+	private HashMap<Location, Integer> spawnTaskID;
+
+	private HashMap<Location, Integer> leashTaskID;
+
+	private HashMap<String, Location> leashedEntityLocations;
+	private HashMap<Location, ArrayList<String>> leashedEntityLists;
 
 	private static SpawnerRegistry registry;
 
@@ -43,5 +51,43 @@ public class SpawnerRegistry {
 
 	public void setMobCount(Location loc, int count){mobCounts.replace(loc, count); }
 
-	//TODO: add leash stuff to registry
+	public void removeMobCount(Location loc){mobCounts.remove(loc);}
+
+	public void addSpawnTaskID(Location loc, int id){spawnTaskID.put(loc, id);}
+
+	public int getSpawnTaskID(Location loc){return spawnTaskID.get(loc);}
+
+	public void removeSpawnTaskID(Location loc){spawnTaskID.remove(loc);}
+
+	public void addLeashTaskID(Location loc, int id){leashTaskID.put(loc, id);}
+
+	public int getLeashTaskID(Location loc){return leashTaskID.get(loc);}
+
+	public void removeLeashTaskID(Location loc){leashTaskID.remove(loc);}
+
+	public void addLeashedEntity(Location loc, String id){
+		leashedEntityLocations.put(id, loc);
+		if(!leashedEntityLists.containsKey(loc)){
+			leashedEntityLists.put(loc, new ArrayList<String>());
+		}
+		leashedEntityLists.get(loc).add(id);
+	}
+
+	public ArrayList<String> getLeashedEntityList(Location loc){return leashedEntityLists.get(loc);}
+
+	public Location getLeashedEntitySpawner(String id){return leashedEntityLocations.get(id);}
+
+	public void removeLeashedEntity(String id){
+		Location loc = leashedEntityLocations.get(id);
+		leashedEntityLocations.remove(id);
+		leashedEntityLists.get(loc).remove(id);
+	}
+
+	public void removeSpawnerLeash(Location loc){
+		ArrayList<String> mobs = leashedEntityLists.get(loc);
+		leashedEntityLists.remove(loc);
+		for(String id : mobs){
+			leashedEntityLocations.remove(id);
+		}
+	}
 }
--- src/main/java/parallelmc/parallelutils/custommobs/spawners/SpawnTask.java
+++ src/main/java/parallelmc/parallelutils/custommobs/spawners/SpawnTask.java
@@ -88,23 +88,12 @@ public class SpawnTask extends BukkitRunnable {
     private boolean playerInRange() {
         Collection<? extends Player> online = plugin.getServer().getOnlinePlayers();
         for (Player player : online) {
-            if (distance(location, player.getLocation()) < options.activationRange) {
+            if (SpawnerHelper.distance(location, player.getLocation()) < options.activationRange) {
                 return true;
             }
         }
         return false;
     }
 
-    private double distance(Location loc1, Location loc2) {
-        if (loc1.getWorld().equals(loc2.getWorld())) {
-            double distancex = loc1.getX() - loc2.getX();
-            double distancey = loc1.getY() - loc2.getY();
-            double distancez = loc1.getZ() - loc2.getZ();
-            distancex *= distancex;
-            distancey *= distancey;
-            distancez *= distancez;
-            return Math.sqrt(distancex + distancey + distancez);
-        }
-        return Integer.MAX_VALUE;
-    }
+
 }
