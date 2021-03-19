ee9121e8298657461923ae5f507d64b0fbfd7735
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2021-03-18 23:50:05 -0400

    Show position and time for advancement and change discord library

--- src/main/java/parallelmc/parallelutils/discordintegration/BotManager.java
+++ src/main/java/parallelmc/parallelutils/discordintegration/BotManager.java
@@ -1,12 +1,14 @@
 package parallelmc.parallelutils.discordintegration;
 
-import discord4j.common.util.Snowflake;
-import discord4j.core.DiscordClient;
-import discord4j.rest.entity.RestChannel;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
 import parallelmc.parallelutils.Parallelutils;
 
 import javax.annotation.Nullable;
-import java.time.Duration;
+import javax.security.auth.login.LoginException;
 import java.util.HashMap;
 import java.util.logging.Level;
 
@@ -15,19 +17,27 @@ import java.util.logging.Level;
  */
 public class BotManager {
 
-	private final DiscordClient client;
+	private final JDA client;
 	private final HashMap<String, String> channels;
 
 	private final int NUM_TRIES = 5;
+
+	private boolean ready = false;
 
 	private static BotManager manager;
 
 	/**
 	 * Instantiate a new BotManager with the given Discord bot token
+	 *
 	 * @param token The Discord bot token
 	 */
-	public BotManager(String token) {
-		client = DiscordClient.create(token);
+	public BotManager(String token) throws LoginException {
+		client = JDABuilder.createDefault(token).addEventListeners((EventListener) event -> {
+			if (event instanceof ReadyEvent) {
+				ready = true;
+				Parallelutils.log(Level.INFO, "JDA Ready");
+			}
+		}).build();
 		channels = new HashMap<>();
 
 		if (manager == null) {
@@ -37,6 +47,7 @@ public class BotManager {
 
 	/**
 	 * Returns the singleton instance of BotManager
+	 *
 	 * @return The instance
 	 */
 	@Nullable
@@ -46,8 +57,9 @@ public class BotManager {
 
 	/**
 	 * Add a channel and ID pair to the map of channels. This is used to assign useful names to channel ids in code
+	 *
 	 * @param name The assigned name for the channel id pair
-	 * @param id The id of the channel
+	 * @param id   The id of the channel
 	 */
 	public void addChannel(String name, String id) {
 		channels.put(name, id);
@@ -55,26 +67,22 @@ public class BotManager {
 
 	/**
 	 * Sends a message in the specified channel
+	 *
 	 * @param channel The shortname of the channel to send a message to
 	 * @param message The message that is being sent
 	 * @return Returns true if the message was sent successfully, false otherwise
 	 */
 	public boolean sendMessage(String channel, String message) {
-		RestChannel restChannel = client.getChannelById(Snowflake.of(channels.get(channel)));
-
+		Parallelutils.log(Level.INFO, "Attempting to send a message");
 		boolean success = false;
-
-		for (int i = 0; i < NUM_TRIES; i++) {
-			try {
-				restChannel.createMessage(message).block(Duration.ofSeconds(2));
-			} catch (Exception e) {
-				Parallelutils.log(Level.INFO, "Failed to send message. Trying again.");
-				continue;
+		if (ready) {
+			TextChannel textChannel = client.getTextChannelById(channels.get(channel));
+			if (textChannel != null) {
+				textChannel.sendMessage(message).queue();
+				success = true;
 			}
-			success = true;
-			break;
 		}
 
-		return success;
+		return success; // Yes this is dumb, no I'm not changing it. It's an artifact of old code
 	}
 }
