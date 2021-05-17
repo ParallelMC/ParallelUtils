3c26afad3d6a43b92ae658053c195b2ea106da32
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2021-05-17 14:26:03 -0400

    Update to Java 16, fix error in listspawners, and fix database timeout problem

--- src/main/java/parallelmc/parallelutils/commands/custommobs/ParallelListSpawnersCommand.java
+++ src/main/java/parallelmc/parallelutils/commands/custommobs/ParallelListSpawnersCommand.java
@@ -4,11 +4,14 @@ import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+import parallelmc.parallelutils.Parallelutils;
 import parallelmc.parallelutils.commands.ParallelCommand;
 import parallelmc.parallelutils.commands.permissions.ParallelOrPermission;
 import parallelmc.parallelutils.commands.permissions.ParallelPermission;
 import parallelmc.parallelutils.custommobs.registry.SpawnerRegistry;
 import parallelmc.parallelutils.custommobs.spawners.SpawnerData;
+
+import java.util.logging.Level;
 
 public class ParallelListSpawnersCommand extends ParallelCommand {
 
@@ -29,7 +32,7 @@ public class ParallelListSpawnersCommand extends ParallelCommand {
 			return true;
 		}
 
-		int numPages = (int)Math.ceil((double)data.length / (double)PAGE_SIZE);
+		int numPages = (int) Math.ceil((double) data.length / (double) PAGE_SIZE);
 
 		int page = 1;
 
@@ -42,27 +45,32 @@ public class ParallelListSpawnersCommand extends ParallelCommand {
 			return true;
 		}
 
-		int start = (page-1)*PAGE_SIZE;
-		int end = start+PAGE_SIZE;
+		int start = (page - 1) * PAGE_SIZE;
+		int end = start + PAGE_SIZE;
 
 		if (end > data.length) {
 			end = data.length;
 		}
 
 		StringBuilder sb = new StringBuilder();
-		for (int i=start; i<end; i++) {
-			sb.append("--------------------------------------------\n");
-			sb.append("ID: ").append(data[i].getUuid()).append("\n");
-			sb.append("Type: ").append(data[i].getType()).append("\n");
-			Location location = data[i].getLocation();
-			sb.append("World: ").append(location.getWorld().getName()).append("\n");
-			sb.append("Location: ").append(location.getBlockX())
-					.append(" ").append(location.getBlockY())
-					.append(" ").append(location.getBlockZ()).append("\n");
-			sb.append("HasLeash: ").append(data[i].hasLeash()).append("\n");
+		for (int i = start; i < end; i++) {
+			try {
+				sb.append("--------------------------------------------\n");
+				sb.append("ID: ").append(data[i].getUuid()).append("\n");
+				sb.append("Type: ").append(data[i].getType()).append("\n");
+				Location location = data[i].getLocation();
+				sb.append("World: ").append(location.getWorld().getName()).append("\n");
+				sb.append("Location: ").append(location.getBlockX())
+						.append(" ").append(location.getBlockY())
+						.append(" ").append(location.getBlockZ()).append("\n");
+				sb.append("HasLeash: ").append(data[i].hasLeash()).append("\n");
+			} catch (NullPointerException e) {
+				Parallelutils.log(Level.INFO, "NullPointerException, skipping");
+			}
 		}
 		sb.append("--------------------------------------------\n");
 		sb.append("Page ").append(page).append("/").append(numPages);
+
 
 		sender.sendMessage(sb.toString());
 
