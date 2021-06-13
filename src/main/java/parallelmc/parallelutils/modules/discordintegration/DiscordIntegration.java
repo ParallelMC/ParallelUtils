2774df8c3edd7994896105695e910e8fcc7ab247
jakebacker <jbacker42@gmail.com>
2021-06-12 22:38:49 -0400

    Complete Modularization Part 1

---
+++ src/main/java/parallelmc/parallelutils/modules/discordintegration/DiscordIntegration.java
@@ -0,0 +1,56 @@
+package parallelmc.parallelutils.modules.discordintegration;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import parallelmc.parallelutils.Constants;
+import parallelmc.parallelutils.ParallelModule;
+import parallelmc.parallelutils.Parallelutils;
+
+import javax.security.auth.login.LoginException;
+import java.util.logging.Level;
+
+public class DiscordIntegration implements ParallelModule {
+
+	@Override
+	public void onEnable() {
+		PluginManager manager = Bukkit.getPluginManager();
+		Plugin plugin = manager.getPlugin(Constants.PLUGIN_NAME);
+
+		if (plugin == null) {
+			Parallelutils.log(Level.SEVERE, "Unable to enable CustomMobs. Plugin " + Constants.PLUGIN_NAME + " does not exist!");
+			return;
+		}
+
+		Parallelutils puPlugin = (Parallelutils) plugin;
+
+		FileConfiguration config = puPlugin.getConfig();
+
+		if (!puPlugin.registerModule("DiscordIntegration", this)) {
+			Parallelutils.log(Level.SEVERE, "Unable to register module DiscordIntegration! Module may already be registered. Quitting...");
+			return;
+		}
+
+		String token = config.getString("token");
+		String serverId = config.getString("server_id");
+		String staffId = config.getString("staff_id");
+
+		String staffChannel = config.getString("staff_channel_id");
+
+		// Register Events for the DiscordIntegration Module
+		BotManager botManager = null;
+		try {
+			botManager = new BotManager(token, serverId, staffId);
+			botManager.addChannel("staff", staffChannel);
+			DiscordIntegrationEventRegistrar.registerEvents();
+		} catch (LoginException e) {
+			Parallelutils.log(Level.SEVERE, "Unable to initialize BotManager. Is the token valid?");
+		}
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
---
+++ src/main/java/parallelmc/parallelutils/modules/discordintegration/DiscordIntegrationEventRegistrar.java
@@ -0,0 +1,38 @@
+package parallelmc.parallelutils.modules.discordintegration;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import parallelmc.parallelutils.Constants;
+import parallelmc.parallelutils.Parallelutils;
+
+import java.util.logging.Level;
+
+/**
+ * The class responsible for registering events pertaining to the discord integration module
+ */
+public class DiscordIntegrationEventRegistrar {
+
+	private static boolean hasRegistered = false;
+
+	/**
+	 * When registerEvents is called, all events relevant to the discord integration module are registered
+	 */
+	public static void registerEvents() {
+		if (!hasRegistered) {
+			PluginManager manager = Bukkit.getPluginManager();
+			Plugin plugin = manager.getPlugin(Constants.PLUGIN_NAME);
+
+			if (plugin == null) {
+				Parallelutils.log(Level.SEVERE, "Unable to register events. Plugin " + Constants.PLUGIN_NAME + " does not exist!");
+				return;
+			}
+
+			manager.registerEvents(new AdvancementListener(), plugin);
+			manager.registerEvents(new JoinQuitSuppressorListener(), plugin);
+
+			hasRegistered = true;
+		}
+	}
+
+}
