632aed1dd30f08264ade82a5a5b5174bc8e462af
Ethan <ebrogames@gmail.com>
2021-06-25 01:16:57 -0400

    base implementation + parallel debug effects command

---
+++ src/main/java/parallelmc/parallelutils/modules/effectextender/commands/ParallelEffectsCommand.java
@@ -0,0 +1,48 @@
+package parallelmc.parallelutils.modules.effectextender.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
+import parallelmc.parallelutils.commands.ParallelCommand;
+import parallelmc.parallelutils.commands.permissions.ParallelPermission;
+import parallelmc.parallelutils.modules.effectextender.EffectListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ParallelEffectsCommand extends ParallelCommand {
+
+    public ParallelEffectsCommand() {
+        super("effects", new ParallelPermission("parallelutils.effects"));
+    }
+
+    @Override
+    public boolean execute(@NotNull CommandSender sender, @NotNull Command command, @NotNull String[] args) {
+        if (!hasPermissions(sender)) {
+            return false;
+        }
+
+        if (sender instanceof Player) {
+            Player player = (Player)sender;
+            HashMap<PotionEffectType, Integer> maxes = EffectListener.playerEffects.get(player);
+            if (maxes == null || maxes.values().size() <= 0) {
+                player.sendMessage("You do not have any active effects!");
+                return true;
+            }
+            player.sendMessage("Your current active effect max durations:");
+            maxes.forEach((k, v) -> player.sendMessage(String.format("%s | %d secs", k, v)));
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<String> getTabComplete(@NotNull CommandSender sender, @NotNull String[] args) {
+        return new ArrayList<>();
+    }
+
+
+}
