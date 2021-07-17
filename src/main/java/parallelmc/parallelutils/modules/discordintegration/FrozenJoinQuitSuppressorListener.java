f53eb0fc5f8c4a4fe76f88111a3f0ec6eb0e14d7
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2021-07-16 23:26:24 -0400

    Send message to player when supressed join and bump version :

--- src/main/java/parallelmc/parallelutils/modules/discordintegration/FrozenJoinQuitSuppressorListener.java
+++ src/main/java/parallelmc/parallelutils/modules/discordintegration/FrozenJoinQuitSuppressorListener.java
@@ -1,5 +1,9 @@
 package parallelmc.parallelutils.modules.discordintegration;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.format.TextFormat;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
@@ -10,6 +14,15 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class FrozenJoinQuitSuppressorListener implements Listener {
+
+	private final Component joinMessage;
+
+	public FrozenJoinQuitSuppressorListener() {
+		joinMessage = Component.text("[", NamedTextColor.DARK_AQUA)
+				.append(Component.text("P", NamedTextColor.WHITE, TextDecoration.BOLD))
+				.append(Component.text("]", NamedTextColor.DARK_AQUA))
+				.append(Component.text(" You are currently vanished from ParallelUtils!", NamedTextColor.AQUA));
+	}
 
 	@EventHandler(priority = EventPriority.LOWEST) // This is event priority abuse
 	public void onFrozenJoin(PlayerJoinEvent event) {
@@ -22,6 +35,7 @@ public class FrozenJoinQuitSuppressorListener implements Listener {
 				if (Bukkit.getPluginManager().isPluginEnabled("Essentials")) {
 					Server server = Bukkit.getServer();
 					server.dispatchCommand(server.getConsoleSender(), "v " + player.getName().strip());
+					player.sendMessage(joinMessage);
 				}
 			}
 		}
