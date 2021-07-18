7813278475792b12f19a3fb238541b12038fc42f
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2021-07-18 18:47:46 -0400

    Fixx beehiveinspector and fix logs

--- src/main/java/parallelmc/parallelutils/modules/gamemode4/sunkenTreasure/SunkenTreasure.java
+++ src/main/java/parallelmc/parallelutils/modules/gamemode4/sunkenTreasure/SunkenTreasure.java
@@ -18,7 +18,7 @@ public class SunkenTreasure implements ParallelModule {
 		Plugin plugin = manager.getPlugin(Constants.PLUGIN_NAME);
 
 		if (plugin == null) {
-			Parallelutils.log(Level.SEVERE, "Unable to enable CustomMobs. Plugin " + Constants.PLUGIN_NAME + " does not exist!");
+			Parallelutils.log(Level.SEVERE, "Unable to enable SunkenTreasure. Plugin " + Constants.PLUGIN_NAME + " does not exist!");
 			return;
 		}
 
