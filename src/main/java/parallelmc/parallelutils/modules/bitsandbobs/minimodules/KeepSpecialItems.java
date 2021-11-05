c8ecb30ef2c170d4a2500c82838a20b1c7664488
Diamondback88 <23108617+Diamondback88@users.noreply.github.com>
2021-11-04 22:41:00 -0400

    Trophies and hats no longer drop upon death

---
+++ src/main/java/parallelmc/parallelutils/modules/bitsandbobs/minimodules/KeepSpecialItems.java
@@ -0,0 +1,70 @@
+package parallelmc.parallelutils.modules.bitsandbobs.minimodules;
+
+import net.minecraft.nbt.NBTTagCompound;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_17_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
+
+
+public class KeepSpecialItems implements Listener {
+
+    private final HashMap<UUID, ArrayList<ItemStack>> specialItemsLogger = new HashMap<>();
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        // Creates an empty list for all items to be prevented from dropping
+        ArrayList<ItemStack> preventedDrops = new ArrayList<>();
+        for (ItemStack item : event.getDrops()) {
+            if (item.getType() == Material.PAPER) {
+                net.minecraft.world.item.ItemStack nmsItem = CraftItemStack.asNMSCopy(item); // does this even work lol
+                // Grabs the NMS items compound and checks if it's null
+                NBTTagCompound compound = (nmsItem.hasTag()) ? nmsItem.getTag() : new NBTTagCompound();
+                if (compound.hasKey("CustomHat")) {
+                    preventedDrops.add(item);
+                }
+            } else if (item.getType() == Material.PLAYER_HEAD) {
+                net.minecraft.world.item.ItemStack nmsItem = CraftItemStack.asNMSCopy(item); // does this even work lol
+                // Grabs the NMS items compound and checks if it's null
+                NBTTagCompound compound = (nmsItem.hasTag()) ? nmsItem.getTag() : new NBTTagCompound();
+                if (compound.hasKey("CustomTrophy")) {
+                    preventedDrops.add(item);
+                }
+            }
+        }
+        // If the list of special drops has items in it, add a new player entry to the logger hashmap and remove the
+        // dropped items
+        if (!preventedDrops.isEmpty()) {
+            // Gets the UUID of the player
+            UUID uuid = event.getEntity().getUniqueId();
+            specialItemsLogger.put(uuid, preventedDrops);
+
+            // Removes each special item from the original list of dropped items
+            for (ItemStack item : preventedDrops) {
+                event.getDrops().remove(item);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        // If the special items hashmap contains the player, then give them their special items back and remove them
+        // from the hashmap
+        if (specialItemsLogger.containsKey(event.getPlayer().getUniqueId())) {
+            ArrayList<ItemStack> items = specialItemsLogger.get(event.getPlayer().getUniqueId());
+            for (ItemStack item : items) {
+                event.getPlayer().getInventory().addItem(item);
+            }
+            // Removes the player from the hashmap
+            specialItemsLogger.remove(event.getPlayer().getUniqueId());
+        }
+    }
+}
