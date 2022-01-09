bccaf4d3a7584d7ac990601a2a1cae5a4f7bf39c
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2022-01-09 14:55:22 -0500

    Implement kill message charm

--- src/main/java/parallelmc/parallelutils/modules/charms/handlers/HandlerType.java
+++ src/main/java/parallelmc/parallelutils/modules/charms/handlers/HandlerType.java
@@ -1,5 +1,6 @@
 package parallelmc.parallelutils.modules.charms.handlers;
 
+
 public enum HandlerType {
 	PARTICLE, // This is particularly weird. Needs to be a runnable only running when item is equipped
 	MESSAGE_KILL, // Listen for kill event
