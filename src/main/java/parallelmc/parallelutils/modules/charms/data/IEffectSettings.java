d76c9c2cd609651c6ada4339942e4bed0dabb5e2
jakebacker <jbacker42@gmail.com>
2022-01-12 14:37:38 -0500

    Fix a few bugs and added toString for charms

--- src/main/java/parallelmc/parallelutils/modules/charms/data/IEffectSettings.java
+++ src/main/java/parallelmc/parallelutils/modules/charms/data/IEffectSettings.java
@@ -7,4 +7,26 @@ import java.util.HashMap;
 public interface IEffectSettings {
 
 	HashMap<String, EncapsulatedType> getSettings();
+
+
+	default String string() {
+		HashMap<String, EncapsulatedType> settings = getSettings();
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("{\n\t");
+
+		for (String n : settings.keySet()) {
+			EncapsulatedType val = settings.get(n);
+
+			sb.append(n);
+			sb.append(": ");
+			sb.append(val.getType().name());
+			sb.append(", ");
+			sb.append(val.getVal().toString());
+			sb.append("\n\t");
+		}
+		sb.append("}");
+
+		return sb.toString();
+	}
 }
