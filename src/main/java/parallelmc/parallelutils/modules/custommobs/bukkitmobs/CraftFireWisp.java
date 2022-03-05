23ef2a629d82674fa32293f663b3c0c18ecb996e
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2022-03-04 21:16:26 -0500

    Update to 1.18.2

--- src/main/java/parallelmc/parallelutils/modules/custommobs/bukkitmobs/CraftFireWisp.java
+++ src/main/java/parallelmc/parallelutils/modules/custommobs/bukkitmobs/CraftFireWisp.java
@@ -9,7 +9,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.craftbukkit.v1_18_R1.entity.CraftZombie;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftZombie;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemFlag;
