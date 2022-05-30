f9b6715d3c32e175faf9461937eb57b260672aaf
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2022-05-30 15:53:46 -0400

    Add ParticleStyleKill

---
+++ src/main/java/parallelmc/parallelutils/modules/charms/playerparticles/styles/ParallelStyles.java
@@ -0,0 +1,28 @@
+package parallelmc.parallelutils.modules.charms.playerparticles.styles;
+
+import dev.esophose.playerparticles.event.ParticleStyleRegistrationEvent;
+import dev.esophose.playerparticles.styles.ParticleStyle;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import parallelmc.parallelutils.Parallelutils;
+
+public class ParallelStyles implements Listener {
+
+	public static final ParticleStyle KILL = new ParticleStyleKill();
+
+	public static void initStyles(Parallelutils puPlugin) {
+		PluginManager pluginManager = Bukkit.getPluginManager();
+		// Register event
+		pluginManager.registerEvents(new ParallelStyles(), puPlugin);
+
+		pluginManager.registerEvents((Listener) KILL, puPlugin);
+	}
+
+	@EventHandler(priority = EventPriority.LOW)
+	public void onParticleStyleRegistration(ParticleStyleRegistrationEvent event) {
+		event.registerEventStyle(KILL);
+	}
+}
