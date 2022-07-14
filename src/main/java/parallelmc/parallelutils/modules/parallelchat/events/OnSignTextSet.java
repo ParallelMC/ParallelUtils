01ac1583175cf68ced9998f71ef56f876a34057b
Ethan <ebrogames@gmail.com>
2022-07-14 00:27:18 -0400

    Updated chat and sign permissions

--- src/main/java/parallelmc/parallelutils/modules/parallelchat/events/OnSignTextSet.java
+++ src/main/java/parallelmc/parallelutils/modules/parallelchat/events/OnSignTextSet.java
@@ -1,6 +1,8 @@
 package parallelmc.parallelutils.modules.parallelchat.events;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -10,13 +12,36 @@ import parallelmc.parallelutils.modules.parallelchat.ParallelChat;
 public class OnSignTextSet implements Listener {
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     public void onSignTextSet(SignChangeEvent event) {
-        if (!event.getPlayer().hasPermission("parallelutils.bypass.antislur")) {
+        Player player = event.getPlayer();
+        if (!player.hasPermission("parallelutils.bypass.antislur")) {
             // fuck u paper im not iterating through components
             String text = String.join("\n", event.getLines());
             text = text.toLowerCase().replace(" ", "");
             if (ParallelChat.get().bannedWords.stream().anyMatch(text::contains)) {
                 event.setCancelled(true);
-                ParallelChat.sendParallelMessageTo(event.getPlayer(), "Please do not say that on signs.");
+                ParallelChat.sendParallelMessageTo(player, "Please do not say that on signs.");
+            }
+        }
+        // foreach was acting weird so regular for loop tally-ho
+        // also rip running these checks for every line
+        for (int i = 0; i < event.lines().size(); i++) {
+            // check sign colors permission
+            if (!player.hasPermission("parallelutils.chat.colors")) {
+                event.line(i, event.lines().get(i).replaceText(x -> x.match("&[[0-9][a-f]]").replacement("")));
+            }
+            // check sign hex permission
+            if (!player.hasPermission("parallelutils.chat.hex")) {
+                event.line(i, event.lines().get(i).replaceText(x -> x.match("&#(.{6})").replacement("")));
+            }
+
+            // check sign formats permission
+            if (!player.hasPermission("parallelutils.chat.formats")) {
+                event.line(i, event.lines().get(i).replaceText(x -> x.match("&[[l-o]r]").replacement("")));
+            }
+
+            // check sign magic permission
+            if (!player.hasPermission("parallel.chat.magic")) {
+                event.line(i, event.lines().get(i).replaceText(x -> x.match("&k").replacement("")));
             }
         }
     }
