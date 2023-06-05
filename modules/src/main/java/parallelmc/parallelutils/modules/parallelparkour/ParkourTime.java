3d1a2b73ef863ba088bde4787556e41a757dc256
Ethan <ebrogames@gmail.com>
2023-06-05 00:51:47 -0400

    cache leaderboard on startup and fix database

--- modules/src/main/java/parallelmc/parallelutils/modules/parallelparkour/ParkourTime.java
+++ modules/src/main/java/parallelmc/parallelutils/modules/parallelparkour/ParkourTime.java
@@ -3,4 +3,4 @@ package parallelmc.parallelutils.modules.parallelparkour;
 import java.util.UUID;
 
 /* Helper record to hold times on a parkour course */
-public record ParkourTime(UUID player, long time) { }
+public record ParkourTime(UUID player, String course, long time) { }
