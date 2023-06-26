461a90128c5ca8253a442580fe69021304e935f9
Jake Backer <jbacker42@gmail.com>
2023-06-26 14:03:17 -0400

    Bump version

--- api/src/main/java/parallelmc/parallelutils/Constants.java
+++ api/src/main/java/parallelmc/parallelutils/Constants.java
@@ -2,7 +2,7 @@ package parallelmc.parallelutils;
 
 public class Constants {
 
-	public static final Version VERSION = new Version(3, 5, 0);
+	public static final Version VERSION = new Version(3, 5, 1);
 	public static final String PLUGIN_NAME = "ParallelUtils";
 	public static final String DEFAULT_WORLD = "world2";
 }
