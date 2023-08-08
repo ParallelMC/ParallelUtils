83e8198e6f3b11dd0bc9fefa5d9889851b41bdce
Ethan <ebrogames@gmail.com>
2023-08-08 16:16:29 -0400

    make endrun teleport to a set spawn point

--- modules/src/main/java/parallelmc/parallelutils/modules/parallelparkour/ParkourLayout.java
+++ modules/src/main/java/parallelmc/parallelutils/modules/parallelparkour/ParkourLayout.java
@@ -1,9 +1,11 @@
 package parallelmc.parallelutils.modules.parallelparkour;
 
 import org.bukkit.Location;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public record ParkourLayout(String name, List<Location> positions, boolean allowEffects) {
+public record ParkourLayout(String name, List<Location> positions, boolean allowEffects, Location spawnPos) {
     public void addLocation(Location location) {
         positions.add(location);
     }
