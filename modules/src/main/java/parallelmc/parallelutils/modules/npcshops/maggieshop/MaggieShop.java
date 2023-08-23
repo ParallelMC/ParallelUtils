de600c2a9480d7ffdc2af0fbf4faf47adc0ec1e0
Ethan <ebrogames@gmail.com>
2023-08-22 22:10:18 -0400

    fixes

--- modules/src/main/java/parallelmc/parallelutils/modules/npcshops/maggieshop/MaggieShop.java
+++ modules/src/main/java/parallelmc/parallelutils/modules/npcshops/maggieshop/MaggieShop.java
@@ -1,11 +1,15 @@
 package parallelmc.parallelutils.modules.npcshops.maggieshop;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import parallelmc.parallelutils.ParallelUtils;
 import parallelmc.parallelutils.util.GUIManager;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 // TODO: abstract this to allow other npc shops
 public class MaggieShop {
@@ -18,8 +22,41 @@ public class MaggieShop {
         loadShopCharms();
     }
 
+    // TODO: optimize
     private void loadShopCharms() {
+        File open = new File(puPlugin.getDataFolder(), "maggie_open.yml");
+        File ranked = new File(puPlugin.getDataFolder(), "maggie_ranked.yml");
+        FileConfiguration config = new YamlConfiguration();
+        try {
+            config.load(open);
+        } catch (Exception e) {
+            ParallelUtils.log(Level.SEVERE, "Failed to load maggie_open.yml!");
+            return;
+        }
+        for (String key : config.getKeys(false)) {
+            String id = config.getString(key + ".charm_id");
+            String name = config.getString(key + ".charm_name");
+            List<String> lore = config.getStringList(key + ".lore");
+            int price = config.getInt(key + ".price");
+            openCharms.add(new ShopCharm(id, name, lore, price, null));
+        }
 
+        try {
+            config.load(ranked);
+        } catch (Exception e) {
+            ParallelUtils.log(Level.SEVERE, "Failed to load maggie_ranked.yml!");
+            return;
+        }
+        for (String key : config.getKeys(false)) {
+            String id = config.getString(key + ".charm_id");
+            String name = config.getString(key + ".charm_name");
+            List<String> lore = config.getStringList(key + ".lore");
+            int price = config.getInt(key + ".price");
+            String rank = config.getString(key + ".required_rank");
+            rankedCharms.add(new ShopCharm(id, name, lore, price, rank));
+        }
+
+        ParallelUtils.log(Level.WARNING, "Loaded " + openCharms.size() + " open charms and " + rankedCharms.size() + " ranked charms");
     }
 
     public ShopCharm getOpenCharm(int index) {
