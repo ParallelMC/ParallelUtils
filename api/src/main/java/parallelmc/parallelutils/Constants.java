b50e707f9bcaf4fd59ed7da3cda7b5c44c44ca4e
Diamondback88 <23108617+Diamondback88@users.noreply.github.com>
2024-12-18 15:47:41 -0500

    Add /hat command and hat slot clicking functionality

--- api/src/main/java/parallelmc/parallelutils/Constants.java
+++ api/src/main/java/parallelmc/parallelutils/Constants.java
@@ -1,11 +1,14 @@
 package parallelmc.parallelutils;
 
-import java.util.ArrayList;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class Constants {
 
 	public static final Version VERSION = new Version(4, 4, 0);
 	public static final String PLUGIN_NAME = "ParallelUtils";
+	public static final Component PLUGIN_PREFIX = MiniMessage.miniMessage()
+			.deserialize("<dark_aqua>[<white><bold>P</bold><dark_aqua>] <reset>");
 	public static final String DEFAULT_WORLD = "world2";
 
 	public static final String[] OVERWORLD_TYPE_WORLDS = {"world", "world2", "world_skyteaser"};
---
+++ modules/src/main/java/parallelmc/parallelutils/modules/bitsandbobs/minimodules/HatSlotStuff.java
@@ -0,0 +1,39 @@
+package parallelmc.parallelutils.modules.bitsandbobs.minimodules;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import parallelmc.parallelutils.util.MessageTools;
+import parallelmc.parallelutils.util.MessageType;
+
+public class HatSlotStuff implements Listener {
+
+    @EventHandler
+    public void onHelmetSlotClick(InventoryClickEvent event) {
+        // Make sure the player is clicking on the helmet slot with an actual item that isn't already supposed to go
+        // in the head slot
+        if (event.getInventory().getType() == InventoryType.PLAYER &&
+                event.getRawSlot() == 5 &&
+                event.getWhoClicked().getItemOnCursor().getType() != Material.AIR &&
+                event.getWhoClicked().getItemOnCursor().getType().getEquipmentSlot() != EquipmentSlot.HEAD) {
+
+            // Swap the cursor and helmet items
+            Player player = (Player) event.getWhoClicked();
+            ItemStack cursorItem = player.getItemOnCursor();
+            if (player.hasPermission("parallelutils.hat.*") ||
+                    ((cursorItem.getType() == Material.PAPER || cursorItem.getType() == Material.LEATHER_HORSE_ARMOR)
+                      && cursorItem.getItemMeta().hasCustomModelData())) {
+
+                ItemStack helmetItem = player.getInventory().getHelmet();
+                player.getInventory().setHelmet(cursorItem);
+                player.setItemOnCursor(helmetItem);
+                MessageTools.sendMessage(player, "Item set as hat!", MessageType.SUCCESS);
+            }
+        }
+    }
+}
