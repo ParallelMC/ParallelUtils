d9768790eb205a2d251cc36a8c839925027bb674
Diamondback88 <23108617+Diamondback88@users.noreply.github.com>
2025-03-27 22:18:40 -0400

    Make the function more awesome

--- api/src/main/java/parallelmc/parallelutils/util/RandomTools.java
+++ api/src/main/java/parallelmc/parallelutils/util/RandomTools.java
@@ -11,11 +11,10 @@ public class RandomTools {
      * Calculates a random integer between two integer values
      * @param low The first integer
      * @param high The second integer
-     * @return The random integer between low (inclusive) and high (exclusive)
-     * For example, if low = 3 and high = 6, possible results could be 3, 4, or 5
+     * @return The random integer between low and high, inclusive
      */
     public static int betweenTwoIntegers(int low, int high) {
         Random r = new Random();
-        return r.nextInt(high-low) + low;
+        return r.nextInt(high - low + 1) + low;
     }
 }
