a8ce779ce4d50e2b0f0bed3783f62636385e252d
Diamondback88 <23108617+Diamondback88@users.noreply.github.com>
2025-03-28 17:31:53 -0400

    Make random variable initialized only once

--- api/src/main/java/parallelmc/parallelutils/util/RandomTools.java
+++ api/src/main/java/parallelmc/parallelutils/util/RandomTools.java
@@ -7,6 +7,12 @@ import java.util.Random;
  */
 public class RandomTools {
 
+    private static Random r;
+
+    private RandomTools() {
+        r = new Random();
+    }
+
     /**
      * Calculates a random integer between two integer values
      * @param low The first integer
@@ -14,7 +20,6 @@ public class RandomTools {
      * @return The random integer between low and high, inclusive
      */
     public static int betweenTwoNumbers(int low, int high) {
-        Random r = new Random();
         return r.nextInt(high - low + 1) + low;
     }
 }
