6517223f9d6f0a5529a9d6a27eba1550d66b5445
Diamondback88 <23108617+Diamondback88@users.noreply.github.com>
2025-04-23 16:16:12 -0400

    Initial 1.21.5 update code, update PaperVanillaGoal to PaperGoal

--- api/src/main/java/parallelmc/parallelutils/Constants.java
+++ api/src/main/java/parallelmc/parallelutils/Constants.java
@@ -5,7 +5,7 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class Constants {
 
-	public static final Version VERSION = new Version(4, 6, 0);
+	public static final Version VERSION = new Version(4, 7, 0);
 	public static final String PLUGIN_NAME = "ParallelUtils";
 	public static final Component PLUGIN_PREFIX = MiniMessage.miniMessage()
 			.deserialize("<dark_aqua>[<white><bold>P</bold><dark_aqua>] <reset>");
