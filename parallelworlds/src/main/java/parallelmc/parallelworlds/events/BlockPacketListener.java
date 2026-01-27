15936001d6f258b5cc2dc7ed8980fcd05f3bda59
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2026-01-27 16:32:17 -0500

    Re-fix noteblocks

--- parallelworlds/src/main/java/parallelmc/parallelworlds/events/BlockPacketListener.java
+++ parallelworlds/src/main/java/parallelmc/parallelworlds/events/BlockPacketListener.java
@@ -34,13 +34,17 @@ public class BlockPacketListener implements PacketListener {
 
     private final int firstCustomId;
 
+    private final int noteblockStart;
+    private final int noteblockEnd; // Last valid noteblock state
     private final int defaultReplaceState;
 
     private final ParallelBlockRegistry registry;
 
     public BlockPacketListener(int firstCustomId) {
         this.firstCustomId = firstCustomId;
-        this.defaultReplaceState = Block.BLOCK_STATE_REGISTRY.getId(Blocks.NOTE_BLOCK.defaultBlockState());
+        this.noteblockStart = Block.BLOCK_STATE_REGISTRY.getId(Blocks.NOTE_BLOCK.defaultBlockState());
+        this.noteblockEnd = this.noteblockStart + Blocks.NOTE_BLOCK.getStateDefinition().getPossibleStates().size() -1;
+        this.defaultReplaceState = noteblockStart;
         this.registry = ParallelBlockRegistry.getInstance();
     }
 
@@ -58,70 +62,82 @@ public class BlockPacketListener implements PacketListener {
                     for (int y = 0; y < 16; y++) {
                         for (int z = 0; z < 16; z++) {
                             int id = c.getBlockId(x, y, z);
+                            Integer replace_state;
                             if (id >= firstCustomId) {
                                 //BlockState state = Block.BLOCK_STATE_REGISTRY.byId(id);
                                 //Logger.getGlobal().log(Level.WARNING, state.toString());
-                                Integer replace_state = registry.getMappedState(id);
+                                replace_state = registry.getMappedState(id);
                                 if (replace_state == null) {
                                     replace_state = defaultReplaceState;
                                     Logger.getGlobal().log(Level.WARNING, "Could not find mapping for id" + id);
                                 }
-
-                                c.set(x, y, z, replace_state);
-
-                                Chunk_v1_18 chunkV118 = (Chunk_v1_18) c;
-
-                                DataPalette palette = getPrivateField("chunkData", Chunk_v1_18.class, chunkV118, DataPalette.class);
-                                ListPalette lp = (ListPalette) palette.palette;
-
-                                int[] palatteData = getPrivateField("data", ListPalette.class, lp, int[].class);
-
-                                for (int i = 0; i< palatteData.length; i++) {
-                                    if (palatteData[i] >= firstCustomId) {
-                                        palatteData[i] = replace_state;
-                                    }
-                                }
-
-
-                                event.markForReEncode(true);
+                            } else if (id >= noteblockStart && id <= noteblockEnd) {
+                                replace_state = noteblockStart;
+                            } else {
+                                continue;
                             }
+                            c.set(x, y, z, replace_state);
+
+                            Chunk_v1_18 chunkV118 = (Chunk_v1_18) c;
+
+                            DataPalette palette = getPrivateField("chunkData", Chunk_v1_18.class, chunkV118, DataPalette.class);
+                            ListPalette lp = (ListPalette) palette.palette;
+
+                            int[] palatteData = getPrivateField("data", ListPalette.class, lp, int[].class);
+
+                            for (int i = 0; i< palatteData.length; i++) {
+                                if (palatteData[i] >= firstCustomId) {
+                                    palatteData[i] = replace_state;
+                                }
+                            }
+
+
+                            event.markForReEncode(true);
                         }
                     }
                 }
             }
-
-            int a = 0;
-
         } else if (event.getPacketType() == PacketType.Play.Server.BLOCK_CHANGE) {
             WrapperPlayServerBlockChange packet = new WrapperPlayServerBlockChange(event);
 
             int id = packet.getBlockId();
+            Integer replace_state;
             if (id >= firstCustomId) {
-                Integer replace_state = registry.getMappedState(id);
+                replace_state = registry.getMappedState(id);
                 if (replace_state == null) {
                     replace_state = defaultReplaceState;
                     Logger.getGlobal().log(Level.WARNING, "Could not find mapping for id" + id);
                 }
-
-                packet.setBlockID(replace_state);
-                event.markForReEncode(true);
+            } else if (id >= noteblockStart && id <= noteblockEnd) {
+                replace_state = noteblockStart;
+            } else {
+                return;
             }
+
+            packet.setBlockID(replace_state);
+            event.markForReEncode(true);
 
             //Logger.getGlobal().log(Level.WARNING, String.valueOf(packet.getBlockId()));
         } else if (event.getPacketType() == PacketType.Play.Server.BLOCK_ACTION) {
             WrapperPlayServerBlockAction packet = new WrapperPlayServerBlockAction(event);
 
             int id = packet.getBlockTypeId();
+            Integer replace_state;
             if (id >= firstCustomId) {
-                Integer replace_state = registry.getMappedState(id);
-                if (replace_state == null) {
-                    replace_state = defaultReplaceState;
-                    Logger.getGlobal().log(Level.WARNING, "Could not find mapping for id" + id);
-                }
-
-                packet.setBlockTypeId(replace_state);
-                event.markForReEncode(true);
+                replace_state = registry.getMappedState(id);
+            }  else if (id >= noteblockStart && id <= noteblockEnd) {
+                replace_state = noteblockStart;
+            } else {
+                return;
             }
+
+            if (replace_state == null) {
+                replace_state = defaultReplaceState;
+                Logger.getGlobal().log(Level.WARNING, "Could not find mapping for id" + id);
+            }
+
+            packet.setBlockTypeId(replace_state);
+            event.markForReEncode(true);
         } else if (event.getPacketType() == PacketType.Play.Server.DECLARE_COMMANDS) {
             WrapperPlayServerDeclareCommands packet = new WrapperPlayServerDeclareCommands(event);
 
